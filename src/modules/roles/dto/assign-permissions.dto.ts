dc5013d3b69f2acad33fae0e2a8b4ba6064c330d
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-07-29 15:41:07 +0500

    fix: allow empty permissionCodes array in AssignPermissionsDto

--- src/modules/roles/dto/assign-permissions.dto.ts
+++ src/modules/roles/dto/assign-permissions.dto.ts
@@ -2,7 +2,6 @@ import { ArrayNotEmpty, IsArray, IsString } from 'class-validator';
 
 export class AssignPermissionsDto {
   @IsArray()
-  @ArrayNotEmpty()
   @IsString({ each: true })
   permissionCodes: string[];
 }
